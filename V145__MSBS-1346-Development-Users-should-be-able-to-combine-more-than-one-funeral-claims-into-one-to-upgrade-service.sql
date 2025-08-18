8b61255ee93dcc4bb9aa3722328bdc5c65d543df
TebogoMohale <tebogo.mohale@mawa.co.za>
2025-08-18 11:02:20 +0200

    MSBS-1346-Add-field-options-for-merging-funeral-claims

---
+++ V145__MSBS-1346-Development-Users-should-be-able-to-combine-more-than-one-funeral-claims-into-one-to-upgrade-service.sql
@@ -0,0 +1,8 @@
+INSERT INTO `field_option` (`code`, `field`, `description`, `type`, `valid_from`, `valid_to`) 
+VALUES ('MERGER', 'CLAIM-TYPE', 'Merger', 'SYSTEM', '2023-09-14', '9999-12-31');
+
+INSERT INTO `field_option` (`code`, `field`, `description`, `type`, `valid_from`, `valid_to`) 
+VALUES ('MERGE-VALUE', 'PRODUCT-ATTRIBUTE', 'Merge Value', 'SYSTEM', '2023-09-14', '9999-12-31');
+
+INSERT INTO `field_option` (`code`, `field`, `description`, `type`, `valid_from`, `valid_to`) 
+VALUES ('FUNERAL-CLAIM-MERGE', 'PAYMENT-REASON', 'Funeral Claim Merge', 'SYSTEM', '2023-09-14', '9999-12-31');
