6e824063b4fa0cb6d2f6a4890f215e91bdb1c8b2
TebogoMohale <tebogo.mohale@mawa.co.za>
2025-08-18 23:02:17 +0200

    MSBS-1346-Update-field-options-for-combining-funeral-claims-and-remove-obsolete-entries

--- V146__MSBS-1346-Development-Users-should-be-able-to-combine-more-than-one-funeral-claims-into-one-to-upgrade-service.sql
+++ V146__MSBS-1346-Development-Users-should-be-able-to-combine-more-than-one-funeral-claims-into-one-to-upgrade-service.sql
@@ -6,3 +6,8 @@ VALUES ('COMBINE-VALUE', 'PRODUCT-ATTRIBUTE', 'Combine Value', 'SYSTEM', '2023-0
 
 INSERT INTO `field_option` (`code`, `field`, `description`, `type`, `valid_from`, `valid_to`) 
 VALUES ('FUNERAL-CLAIM-COMBINE', 'PAYMENT-REASON', 'Funeral Claim Combine', 'SYSTEM', '2023-09-14', '9999-12-31');
+
+DELETE FROM `field_option` WHERE (`code` = 'MERGER') and (`field` = 'CLAIM-TYPE') and (`type` = 'SYSTEM');
+DELETE FROM `field_option` WHERE (`code` = 'MERGE-VALUE') and (`field` = 'PRODUCT-ATTRIBUTE') and (`type` = 'SYSTEM');
+DELETE FROM `field_option` WHERE (`code` = 'FUNERAL-CLAIM-MERGE') and (`field` = 'PAYMENT-REASON') and (`type` = 'SYSTEM');
+
